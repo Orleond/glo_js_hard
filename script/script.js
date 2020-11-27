8785b64bc3527a4a93459f371a8513a10b0ce8fa
Mr. Orleond <study.mr.orleond@gmail.com>
2020-11-27 10:36:28 +0300

    Урок 5 edit

--- script/script.js
+++ script/script.js
@@ -5,6 +5,16 @@ const userString = function(arg) {
     return (typeof arg !== 'string') ? 'Переданный аргумент не является строкой' : (arg = arg.trim(),
         ((arg.length > 30) ? ((arg = arg.substr(0, 30)), (arg += '...')) : arg));
 
-};
+    // if (typeof arg !== 'string') {
+    //     return 'Переданный аргумент не является строкой';
+    // } else {
+    //     arg = arg.trim();
+    //     if (arg.length > 30) {
+    //         arg = arg.substr(0, 30);
+    //         arg += '...';
+    //     }
+    //     return arg;
+    // }
+}
 
-console.log(userString(1));
+console.log(userString('  Длина1Длина2Длина3Длина4Длина5Длина6Длина7    '));
